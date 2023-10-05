e4f607d6e14e943b5c6e2bba8ba14fc134858188
RebecaSpinoza <beca.spinoza@gmail.com>
2023-10-05 19:35:43 -0300

    criando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { ItemResumoComponent } from './item-resumo/item-resumo.component';
 import { ListaDesejosComponent } from './lista-desejos/lista-desejos.component';
 import { ItemPromocaoComponent } from './item-promocao/item-promocao.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemResumoComponent,
     ListaDesejosComponent,
-    ItemPromocaoComponent
+    ItemPromocaoComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
